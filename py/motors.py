1d4bfa43eeb59f88c82ee83631f115e9e57ba008
Yurii Chernyshov <chernyshov.yuriy@gmail.com>
2018-01-27 20:34:09 -0500

    Manage Motors class

--- py/motors.py
+++ py/motors.py
@@ -1,9 +1,15 @@
 import sys
+from enum import Enum
 from os.path import dirname, abspath
 
 sys.path.append(dirname(dirname(abspath(__file__))))
 
 import py.config
+
+
+class MotorsState(Enum):
+    STOPPED = 1
+    STARTED = 2
 
 
 class Motors:
@@ -13,14 +19,19 @@ class Motors:
 
     def __init__(self, on_motors_stopped_in, on_motors_started_in):
         print("Init  motors on %s" % py.config.CONFIG)
+        self.state = MotorsState.STOPPED
         self.on_motors_stopped_ref = on_motors_stopped_in
         self.on_motors_started_ref = on_motors_started_in
 
     def on_echo(self, distance):
-        print("Distance: %d" % distance)
+        # print("Distance: %d" % distance)
         if distance < Motors.min_stop_distance:
-            print("Motor -> stop")
+            if self.state is MotorsState.STOPPED:
+                return
+            self.state = MotorsState.STOPPED
             self.on_motors_stopped_ref()
         else:
-            print("Motor -> start")
+            if self.state is MotorsState.STARTED:
+                return
+            self.state = MotorsState.STARTED
             self.on_motors_started_ref()
