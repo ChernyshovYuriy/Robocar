663879616ab466bfe5f81e9c5e8cd06ea9ad20f0
Yurii Chernyshov <chernyshov.yuriy@gmail.com>
2018-03-18 10:51:52 -0400

    Manage I2C channels

--- py/main.py
+++ py/main.py
@@ -1,5 +1,8 @@
 import sys
 from os.path import dirname, abspath
+from time import sleep
+
+from py.Adafruit_MCP230xx import Adafruit_MCP230XX
 
 sys.path.append(dirname(dirname(abspath(__file__))))
 
@@ -29,6 +32,32 @@ class Controller:
         self.motors = Motors(
             self.on_motors_stopped, self.on_motors_started, self.on_motors_turning
         )
+
+        # ***************************************************
+        # Set num_gpios to 8 for MCP23008 or 16 for MCP23017!
+        # ***************************************************
+        mcp = Adafruit_MCP230XX(address=0x20, num_gpios=16)  # MCP23008
+        # mcp = Adafruit_MCP230XX(address = 0x20, num_gpios = 16) # MCP23017
+
+        # Set pins 0, 1 and 2 to output (you can set pins 0..15 this way)
+        mcp.config(0, mcp.OUTPUT)
+        mcp.config(1, mcp.OUTPUT)
+        mcp.config(2, mcp.OUTPUT)
+
+        # Set pin 3 to input with the pullup resistor enabled
+        mcp.config(3, mcp.INPUT)
+        mcp.pullup(3, 1)
+
+        # Read input pin and display the results
+        print("Pin 3 = %d" % (mcp.input(3) >> 3))
+
+        # Python speed test on output 0 toggling at max speed
+        print("Starting blinky on pin 0 (CTRL+C to quit)")
+        while (True):
+            mcp.output(0, 1)  # Pin 0 High
+            sleep(3)
+            mcp.output(0, 0)  # Pin 0 Low
+            sleep(3)
 
 
     # Start controller.
@@ -109,7 +138,7 @@ class Controller:
 if __name__ == "__main__":
     print("Robocar started on %s, commander is %s" % (py.config.CONFIG, config.COMMANDER))
     GPIOManager.init()
-    I2CManager.init()
+    # I2CManager.init()
 
     distance_prompt = None
     motors_prompt = None
@@ -125,6 +154,6 @@ if __name__ == "__main__":
         commander = UiCommander(controller, root, distance_prompt, motors_prompt)
 
     GPIOManager.cleanup()
-    I2CManager.cleanup()
+    # I2CManager.cleanup()
 
     print("Robocar stopped")
