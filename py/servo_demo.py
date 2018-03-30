17c114e4a564dc6b4f44fbec732dbf3764d0130c
Yurii Chernyshov <chernyshov.yuriy@gmail.com>
2018-03-30 18:26:51 -0400

    Manage echo

--- py/servo_demo.py
+++ py/servo_demo.py
@@ -23,36 +23,22 @@ steps = [500, 833, 1166, 1500, 1833, 2166, 2500]
 pi.set_servo_pulsewidth(PIN, 500)
 time.sleep(1)
 
-# while True:
-#     try:
-#         print("Step:%d" % steps[idx])
-#         pi.set_servo_pulsewidth(PIN, steps[idx])
-#         time.sleep(sleep_time)
-#
-#         if direction:
-#             idx += 1
-#         else:
-#             idx -= 1
-#         if idx == len(steps) - 1:
-#             direction = False
-#         if idx == 0:
-#             direction = True
-#     except KeyboardInterrupt:
-#         break
+while True:
+    try:
+        print("Step:%d" % steps[idx])
+        pi.set_servo_pulsewidth(PIN, steps[idx])
+        time.sleep(sleep_time)
 
-for i in range(0, 500):
-    print("Iteration %d, Step:%d" % (i, steps[idx]))
-    pi.set_servo_pulsewidth(PIN, steps[idx])
-    time.sleep(sleep_time)
-
-    if direction:
-        idx += 1
-    else:
-        idx -= 1
-    if idx == len(steps) - 1:
-        direction = False
-    if idx == 0:
-        direction = True
+        if direction:
+            idx += 1
+        else:
+            idx -= 1
+        if idx == len(steps) - 1:
+            direction = False
+        if idx == 0:
+            direction = True
+    except KeyboardInterrupt:
+        break
 
 print("\nTidying up")
 
