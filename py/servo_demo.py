218dd87b378b572a38c5b009e47afb8ed1dc0f60
Yurii Chernyshov <chernyshov.yuriy@gmail.com>
2018-03-30 16:42:03 -0400

    Manage echo

--- py/servo_demo.py
+++ py/servo_demo.py
@@ -15,30 +15,24 @@ pi = pigpio.pi()
 if not pi.connected:
     exit()
 
-sleep_time = 0.1
+sleep_time = 2
+
+pi.set_servo_pulsewidth(PIN, 1500)
+time.sleep(sleep_time)
+
 while True:
 
     try:
-        # pi.set_servo_pulsewidth(PIN, 0)
-        # time.sleep(sleep_time)
-        # pi.set_servo_pulsewidth(PIN, 500)
-        # time.sleep(sleep_time)
         pi.set_servo_pulsewidth(PIN, 1000)
         time.sleep(sleep_time)
         pi.set_servo_pulsewidth(PIN, 1500)
         time.sleep(sleep_time)
         pi.set_servo_pulsewidth(PIN, 2000)
         time.sleep(sleep_time)
-        # pi.set_servo_pulsewidth(PIN, 2500)
-        # time.sleep(sleep_time)
-        # pi.set_servo_pulsewidth(PIN, 2000)
-        # time.sleep(sleep_time)
         pi.set_servo_pulsewidth(PIN, 1500)
         time.sleep(sleep_time)
-        # pi.set_servo_pulsewidth(PIN, 1000)
-        # time.sleep(sleep_time)
-        # pi.set_servo_pulsewidth(PIN, 500)
-        # time.sleep(sleep_time)
+        pi.set_servo_pulsewidth(PIN, 1000)
+        time.sleep(sleep_time)
     except KeyboardInterrupt:
         break
 
