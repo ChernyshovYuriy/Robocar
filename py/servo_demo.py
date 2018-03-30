0d1c37b21889ca5bffeb76bc6b6418c12f83b657
Yurii Chernyshov <chernyshov.yuriy@gmail.com>
2018-03-30 15:42:49 -0400

    Manage echo

--- py/servo_demo.py
+++ py/servo_demo.py
@@ -4,75 +4,34 @@
 # 2016-10-07
 # Public Domain
 
-# servo_demo.py          # Send servo pulses to GPIO 4.
-# servo_demo.py 23 24 25 # Send servo pulses to GPIO 23, 24, 25.
-
-import random
-import sys
 import time
 
 import pigpio
 
-NUM_GPIO = 32
-
-MIN_WIDTH = 1000
-MAX_WIDTH = 2000
-
-step = [0] * NUM_GPIO
-width = [0] * NUM_GPIO
-used = [False] * NUM_GPIO
+PIN = 4
 
 pi = pigpio.pi()
 
 if not pi.connected:
     exit()
 
-if len(sys.argv) == 1:
-    G = [4]
-else:
-    G = []
-    for a in sys.argv[1:]:
-        G.append(int(a))
-
-for g in G:
-    used[g] = True
-    step[g] = random.randrange(5, 25)
-    if step[g] % 2 == 0:
-        step[g] = -step[g]
-    width[g] = random.randrange(MIN_WIDTH, MAX_WIDTH + 1)
-
-print("Sending servos pulses to GPIO {}, control C to stop.".
-      format(' '.join(str(g) for g in G)))
-
 while True:
 
     try:
 
-        # for g in G:
-
-        pi.set_servo_pulsewidth(g, 2000)
-
-        # print(g, width[g])
-        time.sleep(1)
-
-        pi.set_servo_pulsewidth(g, 1000)
+        pi.set_servo_pulsewidth(PIN, 2000)
 
         time.sleep(1)
 
-        # width[g] += step[g]
-        #
-        # if width[g] < MIN_WIDTH or width[g] > MAX_WIDTH:
-        #     step[g] = -step[g]
-        #     width[g] += step[g]
+        pi.set_servo_pulsewidth(PIN, 1000)
 
-        # time.sleep(0.1)
+        time.sleep(1)
 
     except KeyboardInterrupt:
         break
 
 print("\nTidying up")
 
-for g in G:
-    pi.set_servo_pulsewidth(g, 0)
+pi.set_servo_pulsewidth(PIN, 0)
 
 pi.stop()
