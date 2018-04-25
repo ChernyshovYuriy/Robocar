f3073589f1b0470b3b5ba2581019a63ef31b5757
Yurii Chernyshov <chernyshov.yuriy@gmail.com>
2018-04-24 20:28:45 -0400

    LM393

--- py/lm393.py
+++ py/lm393.py
@@ -1,5 +1,6 @@
 import sys
 from os.path import dirname, abspath
+from time import time
 
 sys.path.append(dirname(dirname(abspath(__file__))))
 
@@ -16,10 +17,30 @@ class LM393:
 
     def start(self):
         print("Start LM393")
-        GPIO.add_event_detect(GPIOManager.LM393, GPIO.RISING, callback=self.callback, bouncetime=300)
+        try:
+            # Main loop
+            while True:
+                print ("Count:" % self.callback())
+        except KeyboardInterrupt:
+            pass
 
     def stop(self):
         print("Stop  LM393")
 
     def callback(self):
         print("Callback LM393")
+        count = 0
+
+        # Output on the pin for
+        GPIO.setup(GPIOManager.LM393, GPIO.OUT)
+        GPIO.output(GPIOManager.LM393, GPIO.LOW)
+        time.sleep(0.1)
+
+        # Change the pin back to input
+        GPIO.setup(GPIOManager.LM393, GPIO.IN)
+
+        # Count until the pin goes high
+        while GPIO.input(GPIOManager.LM393) == GPIO.LOW:
+            count += 1
+
+        return count
