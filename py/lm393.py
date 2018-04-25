dfb86e73b2306a79abafd529c0a1f510dcfd8a5c
Yurii Chernyshov <chernyshov.yuriy@gmail.com>
2018-04-24 21:28:01 -0400

    LM393

--- py/lm393.py
+++ py/lm393.py
@@ -34,7 +34,7 @@ class LM393:
         # Output on the pin for
         GPIO.setup(GPIOManager.LM393, GPIO.OUT)
         GPIO.output(GPIOManager.LM393, GPIO.LOW)
-        sleep(0.01)
+        sleep(0.001)
 
         # Change the pin back to input
         GPIO.setup(GPIOManager.LM393, GPIO.IN)
