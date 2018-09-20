ad27ac008b922d4db3ccdbc83a42be5b1751755d
Yurii Chernyshov <chernyshov.yuriy@gmail.com>
2018-09-19 21:28:33 -0400

    Debugging echo ...

--- py/httpserver.py
+++ py/httpserver.py
@@ -63,7 +63,7 @@ class ConnectionHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', mime_type)
         self.end_headers()
         self.wfile.write(bytes("<body><p>This is a test.</p>", "utf-8"))
-        self.wfile.close()
+        # self.wfile.close()
 
 
 class HttpServer:
