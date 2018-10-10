914ba4e3878479be53e5c76d4ee884d3de389a46
Yurii Chernyshov <chernyshov.yuriy@gmail.com>
2018-10-10 18:37:29 -0400

    Handle more echo sensors

--- py/echo.py
+++ py/echo.py
@@ -85,7 +85,7 @@ class Echo:
                     if distance[i] == 0 and self.distance_prev[i] != 0:
                         distance[i] = self.distance_prev[i]
             # self.on_echo(distance)
-            print(" -------> ECHO:".join(distance))
+            print("--------> ECHO %s" % distance)
             sleep(0.05)
 
     # Get distance from sensor.
