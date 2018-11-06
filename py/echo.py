8a6241e27d224d8926fe3f77831bd6e2170d41d7
Yurii Chernyshov <chernyshov.yuriy@gmail.com>
2018-11-05 19:05:24 -0500

    Manage robocar

--- py/echo.py
+++ py/echo.py
@@ -73,7 +73,6 @@ class Echo:
         print("Stop  echo")
         self.is_run = False
         self.thread = None
-        self.on_echo(self.default_distance)
         # self.sonar_sensor.cancel()
         # self.pi.stop()
 
