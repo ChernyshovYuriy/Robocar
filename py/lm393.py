67b821c805bda0b15346af9d9510fdd26d8bf66f
Yurii Chernyshov <chernyshov.yuriy@gmail.com>
2018-11-28 17:46:39 -0500

    Manage robocar

--- py/lm393.py
+++ py/lm393.py
@@ -28,7 +28,6 @@ class LM393:
         self.dist_meas = 0.00
         self.km_per_hour = 0
         self.rpm = 0
-        self.elapse = 0
         self.pulse = 0
         self.start_timer = time.time()
 
@@ -41,7 +40,6 @@ class LM393:
         self.dist_meas = 0.00
         self.km_per_hour = 0
         self.rpm = 0
-        self.elapse = 0
         self.pulse = 0
         self.start_timer = time.time()
         """Run LM393 in separate thread"""
@@ -57,24 +55,25 @@ class LM393:
         self.is_run = False
         self.thread = None
 
-    def calculate(self):
-        if self.elapse != 0:  # to avoid DivisionByZero error
-            self.rpm = 1 / self.elapse * 60
+    def calculate(self, elapse):
+        if elapse != 0:  # to avoid DivisionByZero error
+            self.rpm = 1 / elapse * 60
             circ_cm = (2 * math.pi) * self.r_cm  # calculate wheel circumference in CM
             dist_km = circ_cm / 100000  # convert cm to km
-            km_per_sec = dist_km / self.elapse  # calculate KM/sec
+            km_per_sec = dist_km / elapse  # calculate KM/sec
             km_per_hour = km_per_sec * 3600  # calculate KM/h
             dist_meas = (dist_km * self.pulse) * 1000  # measure distance traverse in meter
-            print('rpm:{0:.0f}-RPM kmh:{1:.0f}-KMH dist_meas:{2:.2f}m pulse:{3}'.format(self.rpm, km_per_hour, dist_meas,
-                                                                                        self.pulse))
+            print('RPM:{0:.0f} Speed:{1:.0f} Km/H Distance:{2:.2f}m Pulse:{3}'.format(self.rpm, km_per_hour, dist_meas,
+                                                                                      self.pulse))
         pass
 
     def right_sensor_callback(self, channel):
-        print("Callback R")
-        self.pulse += 1  # increase pulse by 1 whenever interrupt occurred
-        self.elapse = time.time() - self.start_timer  # elapse for every 1 complete rotation made!
+        # increase pulse by 1 whenever interrupt occurred
+        self.pulse += 1
+        # elapse for every 1 complete rotation made
+        elapse = time.time() - self.start_timer
         self.start_timer = time.time()
-        self.calculate()
+        self.calculate(elapse)
 
     def left_sensor_callback(self, channel):
         print("Callback L")
