52ccb4cbe789f806327faae94ab9ba0ad79ceb9d
Yurii Chernyshov <chernyshov.yuriy@gmail.com>
2018-11-29 08:26:30 -0500

    Manage robocar

--- py/lm393.py
+++ py/lm393.py
@@ -4,18 +4,19 @@ from os.path import dirname, abspath
 sys.path.append(dirname(dirname(abspath(__file__))))
 
 from py.gpio_manager import GPIOManager
-from threading import Thread
 import py.config
 import time, math
 
 if py.config.CONFIG is py.config.Platform.PI:
     import RPi.GPIO as GPIO
 
-# Max counter for the echo back
-LM393_MAX_COUNTER = 10000
-
 
 class LM393:
+
+    NUM_OF_SENSORS = 2
+    LEFT_SENSOR_ID = 0
+    RIGHT_SENSOR_ID = 1
+    SENSORS = [LEFT_SENSOR_ID, RIGHT_SENSOR_ID]
 
     def __init__(self, on_value):
         print("Init  LM393")
@@ -24,12 +25,12 @@ class LM393:
         self.thread = None
         self.on_value_int = on_value
 
-        self.r_cm = 0.2
-        self.dist_meas = [0.00, 0.00]
-        self.km_per_hour = [0, 0]
-        self.rpm = [0, 0]
-        self.pulse = [0, 0]
-        self.start_timer = [time.time(), time.time()]
+        self.wheel_circumference_cm = 21
+        self.dist_meas = [0.00] * LM393.NUM_OF_SENSORS
+        self.km_per_hour = [0] * LM393.NUM_OF_SENSORS
+        self.rpm = [0] * LM393.NUM_OF_SENSORS
+        self.pulse = [0] * LM393.NUM_OF_SENSORS
+        self.start_timer = [time.time()] * LM393.NUM_OF_SENSORS
 
     def start(self):
         print("Start LM393")
@@ -37,50 +38,7 @@ class LM393:
             return
 
         self.is_run = True
-        """Run LM393 in separate thread"""
-        if self.thread is None:
-            self.thread = Thread(target=self.runnable)
-        self.thread.start()
-
-    def stop(self):
-        print("Stop  LM393")
-        if self.is_run is False:
-            return
-
-        self.is_run = False
-        self.thread = None
-
-    def calculate(self, elapse, i):
-        if elapse != 0:  # to avoid DivisionByZero error
-            self.rpm[i] = 1 / elapse * 60
-            circ_cm = (2 * math.pi) * self.r_cm  # calculate wheel circumference in CM
-            dist_km = circ_cm / 100000  # convert cm to km
-            km_per_sec = dist_km / elapse  # calculate KM/sec
-            self.km_per_hour[i] = km_per_sec * 3600  # calculate KM/h
-            self.dist_meas[i] = (dist_km * self.pulse[i]) * 1000  # measure distance traverse in meter
-            print('RPM:{0:.0f} Speed:{1:.0f} Km/H Distance:{2:.2f}m Pulse:{3}'.format(
-                self.rpm[i], self.km_per_hour[i], self.dist_meas[i], self.pulse[i])
-            )
-
-    def right_sensor_callback(self, channel):
-        # increase pulse by 1 whenever interrupt occurred
-        self.pulse[1] += 1
-        # elapse for every 1 complete rotation made
-        elapse = time.time() - self.start_timer[1]
-        self.start_timer[1] = time.time()
-        self.calculate(elapse, 1)
-
-    def left_sensor_callback(self, channel):
-        # increase pulse by 1 whenever interrupt occurred
-        self.pulse[0] += 1
-        # elapse for every 1 complete rotation made
-        elapse = time.time() - self.start_timer[0]
-        self.start_timer[0] = time.time()
-        self.calculate(elapse, 0)
-
-    # Handle distance measurement.
-    def runnable(self):
-        for i in range(2):
+        for i in range(LM393.NUM_OF_SENSORS):
             self.rpm[i] = 0
             self.pulse[i] = 0
             self.dist_meas[i] = 0.00
@@ -88,3 +46,41 @@ class LM393:
             self.start_timer[i] = time.time()
         GPIO.add_event_detect(GPIOManager.LM393_R, GPIO.FALLING, callback=self.right_sensor_callback, bouncetime=20)
         GPIO.add_event_detect(GPIOManager.LM393_L, GPIO.FALLING, callback=self.left_sensor_callback, bouncetime=20)
+
+    def stop(self):
+        print("Stop  LM393")
+        if self.is_run is False:
+            return
+
+        self.is_run = False
+        GPIO.remove_event_detect(GPIOManager.LM393_R)
+        GPIO.remove_event_detect(GPIOManager.LM393_L)
+
+    def calculate(self, elapse, sensor_id):
+        if elapse != 0:  # to avoid DivisionByZero error
+            self.rpm[sensor_id] = 1 / elapse * 60
+            # convert cm to km
+            wheel_circumference_km = self.wheel_circumference_cm / 100000
+            # calculate KM/sec
+            km_per_sec = wheel_circumference_km / elapse  # calculate KM/sec
+            # calculate KM/h
+            self.km_per_hour[sensor_id] = km_per_sec * 3600
+            # measure distance traverse in meter
+            self.dist_meas[sensor_id] = (wheel_circumference_km * self.pulse[sensor_id]) * 1000
+            print('RPM:{0:.0f} Speed:{1:.0f} Km/H Distance:{2:.2f}m Pulse:{3}'.format(
+                self.rpm[sensor_id], self.km_per_hour[sensor_id], self.dist_meas[sensor_id], self.pulse[sensor_id])
+            )
+
+    def handle_callback(self, sensor_id):
+        # increase pulse by 1 whenever interrupt occurred
+        self.pulse[sensor_id] += 1
+        # elapse for every 1 complete rotation made
+        elapse = time.time() - self.start_timer[sensor_id]
+        self.start_timer[sensor_id] = time.time()
+        self.calculate(elapse, sensor_id)
+
+    def right_sensor_callback(self, channel):
+        self.handle_callback(LM393.RIGHT_SENSOR_ID)
+
+    def left_sensor_callback(self, channel):
+        self.handle_callback(LM393.LEFT_SENSOR_ID)
