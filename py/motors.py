06ffca3d8cea8a60cbd484bac5da609a2ea5c469
Yurii Chernyshov <chernyshov.yuriy@gmail.com>
2018-11-29 11:04:24 -0500

    Manage robocar

--- py/motors.py
+++ py/motors.py
@@ -117,12 +117,11 @@ class Motors:
     def stop(self):
         if not self.is_run:
             return
-        print("Stop  motors %d" % threading.get_ident())
+        print("Stop  motors")
         self.is_run = False
         self.stop_motors()
 
     def on_echo(self, distance, weights):
-        print("ECHO %d" % threading.get_ident())
         if not self.is_run:
             new_state = MotorsState.STOP
         else:
