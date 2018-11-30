10a53b6dac504633fea03bebd8652c17bf82dc08
Yurii Chernyshov <chernyshov.yuriy@gmail.com>
2018-11-30 10:07:52 -0500

    Manage robocar

--- py/lm393.py
+++ py/lm393.py
@@ -51,10 +51,10 @@ class LM393:
         """
         if not GPIOManager.IS_LM393_CALLBACK_REGISTERED:
             GPIO.add_event_detect(
-                GPIOManager.LM393_R, GPIO.FALLING, callback=self.right_sensor_callback, bouncetime=100
+                GPIOManager.LM393_R, GPIO.FALLING, callback=self.right_sensor_callback, bouncetime=60
             )
             GPIO.add_event_detect(
-                GPIOManager.LM393_L, GPIO.FALLING, callback=self.left_sensor_callback, bouncetime=100
+                GPIOManager.LM393_L, GPIO.FALLING, callback=self.left_sensor_callback, bouncetime=60
             )
             GPIOManager.IS_LM393_CALLBACK_REGISTERED = True
 
