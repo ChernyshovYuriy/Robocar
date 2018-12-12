69edc24c2041cc27a85d1910fd4e9261f55e586a
Yurii Chernyshov <chernyshov.yuriy@gmail.com>
2018-12-12 18:53:29 -0500

    Manage robocar's echo

--- py/lm393.py
+++ py/lm393.py
@@ -60,8 +60,8 @@ class LM393:
                 GPIOManager.LM393_L, GPIO.FALLING, callback=self.left_sensor_callback, bouncetime=100
             )
             GPIOManager.IS_LM393_CALLBACK_REGISTERED = True
-            print("Start LM393 listening")
-            self.handle_timer(True)
+        print("Start LM393 listening")
+        self.handle_timer(True)
 
     def stop(self):
         if self.is_run is False:
