2d0afb1a7417885bbcb23c074241d23877c3013a
Yurii Chernyshov <chernyshov.yuriy@gmail.com>
2018-12-12 13:18:55 -0500

    Manage robocar's echo

--- py/echo.py
+++ py/echo.py
@@ -76,7 +76,7 @@ class Echo:
                 weights[i] = distance[i]
             self.calculate_weights(weights)
             self.on_echo(distance, weights)
-            sleep(0.05)
+            sleep(0.1)
 
     def calculate_weights(self, weights):
         """
